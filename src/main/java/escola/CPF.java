274dd4a627a2bccea11053c49aeeb7e293661d3e
Luiz Almeida <luizorst@hotmail.com>
2021-05-04 11:07:05 -0300

    [ADD] Basic testing for CPF class

--- src/main/java/escola/CPF.java
+++ src/main/java/escola/CPF.java
@@ -3,8 +3,12 @@ package escola;
 public class CPF {
     private String numero;
 
+    public String getNumberString() {
+        return numero;
+    }
+
     public CPF(String numero) {
-        if (numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+        if (numero == null || !(numero.matches("\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}"))) {
             throw new IllegalArgumentException("CPF invalido");
         }
         this.numero = numero;
