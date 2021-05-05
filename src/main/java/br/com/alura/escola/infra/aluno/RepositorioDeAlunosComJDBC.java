8d6bca304aad608f8afe4fc8b93fb115c3753f5e
Luiz Almeida <luizorst@hotmail.com>
2021-05-05 09:16:36 -0300

    [ADD] get all students to repository

--- src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
+++ src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
@@ -75,7 +76,21 @@ public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 
     @Override
     public List<Aluno> listarTodosAlunosMatriculados() {
-        return null;
+        try {
+            String sql = "SELECT id, cpf, nome, email FROM aluno";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            List<Aluno> alunos = new ArrayList<>();
+            while (rs.next()) {
+                CPF cpf = new CPF(rs.getString(("cpf")));
+                String nome = rs.getString("nome");
+                Email email = new Email(rs.getString("email"));
+                Aluno aluno = new Aluno(cpf, nome, email);
+                alunos.add(aluno);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 }
